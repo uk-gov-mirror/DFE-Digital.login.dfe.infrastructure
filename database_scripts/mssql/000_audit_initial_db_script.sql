1c5b810cbc3f139e3faba46b9c67de0dceabe133
David Goodyear <dave@outka5t.co.uk>
2018-02-19 23:10:21 +0000

    Azure SQL and Databases
    Added structure for scripts
    Added sql to shared template
    Added databases to shared template

---
+++ database_scripts/mssql/000_audit_initial_db_script.sql
@@ -0,0 +1,15 @@
+If NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'AuditLogs')
+    BEGIN
+    CREATE TABLE AuditLogs
+    (
+        id int PRIMARY KEY NOT NULL IDENTITY,
+        level nvarchar(255),
+        message nvarchar(255),
+        meta nvarchar(max),
+        createdAt datetimeoffset NOT NULL,
+        updatedAt datetimeoffset NOT NULL,
+        application varchar(255),
+        environment varchar(255)
+    );
+    CREATE INDEX level ON [lgn-dev-dir-db].dbo.AuditLogs (level);
+END
