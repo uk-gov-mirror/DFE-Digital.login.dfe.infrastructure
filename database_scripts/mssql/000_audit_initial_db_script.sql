4de3d88d4968edeac7df2342efd4b00ca6fd6b75
Simon Bull <simonbu11@users.noreply.github.com>
2018-02-22 16:02:51 +0000

    Update 000_audit_initial_db_script.sql
    Update script for unpacked metadata

--- database_scripts/mssql/000_audit_initial_db_script.sql
+++ database_scripts/mssql/000_audit_initial_db_script.sql
@@ -1,15 +1,33 @@
+
 IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'AuditLogs')
     BEGIN
     CREATE TABLE AuditLogs
     (
-        id int PRIMARY KEY NOT NULL IDENTITY,
+        id uniqueidentifier PRIMARY KEY NOT NULL,
         level nvarchar(255),
-        message nvarchar(255),
-        meta nvarchar(max),
+        message nvarchar(max),
         createdAt datetimeoffset NOT NULL,
         updatedAt datetimeoffset NOT NULL,
         application varchar(255),
-        environment varchar(255)
+        environment varchar(255),
+        type varchar(255),
+        subType varchar(255),
+        userId uniqueidentifier
     );
     CREATE INDEX level ON dbo.AuditLogs (level);
+    CREATE INDEX userId ON dbo.AuditLogs (userId);
+    CREATE INDEX type ON dbo.AuditLogs (type);
+END
+
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'AuditLogMeta')
+    BEGIN
+    CREATE TABLE AuditLogMeta
+    (
+        id uniqueidentifier PRIMARY KEY NOT NULL,
+        auditId uniqueidentifier NOT NULL,
+        [key] nvarchar(125) NOT NULL,
+        [value] nvarchar(max) NULL,
+        CONSTRAINT [AuditLog_FK] FOREIGN KEY (auditId) REFERENCES AuditLogs(id)
+    );
+    CREATE INDEX audit_key_value ON dbo.AuditLogMeta (auditId, [key], [value]);
 END
