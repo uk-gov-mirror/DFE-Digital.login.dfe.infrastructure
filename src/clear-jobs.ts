658bab4dfc48fdb2ec7f75c7400b6cffe78aa7ea
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2020-03-26 07:31:19 +0000

    started adding redis

--- src/clear-jobs.ts
+++ src/clear-jobs.ts
@@ -1,15 +1,18 @@
 import config from "./config";
+
 import { request } from "https";
 import { IncomingWebhook } from "@slack/webhook";
+import Redis from 'ioredis';
 
 const jwtStrategy = require("login.dfe.jwt-strategies");
+const redis = new Redis(process.env.REDIS_STRING);
 
 // generate token
 async function getToken(): Promise<string> {
   return await jwtStrategy(config.token).getBearerToken();
 }
 
-function makeRequest(token: string, body: string): Promise<any> {
+function makeRequest(token: string, body: string): Promise<void> {
   const DATA = JSON.stringify({
     state: body
   });
@@ -29,9 +32,9 @@ function makeRequest(token: string, body: string): Promise<any> {
 
     const req = request(options, res => {
       if (res.statusCode === 200 || res.statusCode === 304) {
-        resolve(res);
+        resolve();
       } else {
-        reject(res);
+        reject();
       }
     });
 
@@ -44,49 +47,56 @@ function makeRequest(token: string, body: string): Promise<any> {
   });
 }
 
-function sleep(ms: number = 10000): Promise<void> {
+function sleep(ms: number = 1000): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function notify(title: string, msg: string): Promise<void> {
   return new Promise(async (resolve, reject) => {
-    const webhook = new IncomingWebhook("");
+    try {
+      const webhook = new IncomingWebhook(process.env.SLACK_AUTOJOB_WEBOOK);
 
-    let a = await webhook.send({
-      attachments: [
-        {
-          fallback: "Clear jobs ran",
-          color: "#36a64f",
-          author_name: "dfe.login.infrastructure",
-          title: title,
-          text: msg,
-          footer: "Archronos",
-          footer_icon:
-            "http://archronos.com/wp-content/uploads/2019/02/square-1.png",
-          ts: `${Date.now()}`
-        }
-      ]
-    });
+      await webhook.send({
+        attachments: [
+          {
+            fallback: "Clear jobs ran",
+            color: "#36a64f",
+            author_name: "dfe.login.infrastructure",
+            title: title,
+            text: msg,
+            footer: "Archronos",
+            footer_icon:
+              "http://archronos.com/wp-content/uploads/2019/02/square-1.png",
+            ts: `${Date.now()}`
+          }
+        ]
+      });
 
-    console.log(a);
-
-    resolve();
+      resolve();
+    } catch (e) {
+      reject();
+    }
   });
 }
 
 (async function run(): Promise<void> {
+
+
+
+
   let errorCode;
   try {
     let token = await getToken();
     await makeRequest(token, "stopped");
     // await notify("Jobs stopped", "KueJS jobs stopped");
-    await sleep(config.wait);
+    await sleep();
     await makeRequest(token, "started");
-    await notify("Jobs started", "KueJS jobs started");
+    // await notify("Jobs started", "KueJS jobs started");
   } catch (e) {
     console.error(e);
     errorCode = 1;
   } finally {
+    console.log(`completed at ${new Date()}`)
     process.exit(errorCode);
   }
 })();
