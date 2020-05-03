f9f7bd13485a5278cba37e154c39b0670aa3e70c
kalimakbar <62335147+kalimakbar@users.noreply.github.com>
2020-05-03 12:32:55 +0100

    Create migrate_last_months_data.sql
    move last 3 month data back to  original tables - this is a one off script!

---
+++ database_scripts/mssql/migrate_last_months_data.sql
@@ -0,0 +1,47 @@
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'AuditLogs')
+    BEGIN
+    CREATE TABLE AuditLogs
+    (
+        id uniqueidentifier NOT NULL,
+        level nvarchar(255),
+        message nvarchar(max),
+        createdAt datetimeoffset NOT NULL,
+        updatedAt datetimeoffset NOT NULL,
+        application varchar(255),
+        environment varchar(255),
+        type varchar(255),
+        subType varchar(255),
+        userId uniqueidentifier,
+        organisationid uniqueidentifier
+    );
+END
+---------------------------------------------
+BEGIN TRANSACTION COPYAUDITDATA;
+INSERT INTO dbo.AuditLogs
+SELECT * FROM dbo.AuditLogsBackup WHERE createdAt >= DATEADD(MONTH, -3, GETDATE());
+DELETE FROM dbo.AuditLogsBackup WHERE createdAt >= DATEADD(MONTH, -3, GETDATE());
+COMMIT TRANSACTION COPYAUDITDATA;
+---------------------------------------------
+ALTER TABLE dbo.AuditLogs add primary key (id);
+CREATE INDEX level ON dbo.AuditLogs (level);
+CREATE INDEX userId ON dbo.AuditLogs (userId);
+CREATE INDEX type ON dbo.AuditLogs (type);
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'AuditLogMeta')
+    BEGIN
+    CREATE TABLE AuditLogMeta
+    (
+        id uniqueidentifier NOT NULL,
+        auditId uniqueidentifier NOT NULL,
+        [key] nvarchar(125) NOT NULL,
+        [value] nvarchar(max) NULL,
+        CONSTRAINT [AuditLog_FK2] FOREIGN KEY (auditId) REFERENCES AuditLogs(id)
+    );
+END
+--------------------------
+BEGIN TRANSACTION COPYAUDITMETADATA;
+INSERT INTO dbo.AuditLogMeta (id,auditId, "key","value")
+SELECT almb.[id], almb.[auditId], almb.[key], almb.[value] FROM AuditLogMetaBackUp almb INNER JOIN dbo.AuditLogs al ON almb.auditId = al.id;
+DELETE almb
+FROM AuditLogMetaBackUp almb
+INNER JOIN dbo.AuditLogs al ON almb.auditId = al.id;
+COMMIT TRANSACTION COPYAUDITMETADATA;
