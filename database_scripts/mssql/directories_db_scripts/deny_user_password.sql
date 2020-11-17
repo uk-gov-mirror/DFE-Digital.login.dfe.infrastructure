642d5df557324f7a1bb6b12a056f3b110d19dc0d
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2020-11-17 18:22:13 +0000

    Update deny_user_password.sql

--- database_scripts/mssql/directories_db_scripts/deny_user_password.sql
+++ database_scripts/mssql/directories_db_scripts/deny_user_password.sql
@@ -1,1 +1,1 @@
-DENY SELECT ON dbo.[user] ([password]) TO dadan_dfe,jgarcia_dfe,jjoy_dfe,scolemanwilliams_dfe,mesen_dfe;
+DENY SELECT ON dbo.[user] ([password]) TO dadan_dfe,jgarcia_dfe,jjoy_dfe,scolemanwilliams_dfe,mesen_dfe,ssarkar_dfe
